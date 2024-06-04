de0e1f043113a4a44452d2ce4d621e8c54c63b66
Pmquan <quan111220@gmail.com>
2024-06-04 22:33:23 +0700

    feat-test-commit

--- frontend/newsfeed-ui/src/components/header/index.tsx
+++ frontend/newsfeed-ui/src/components/header/index.tsx
@@ -1,7 +1,28 @@
-import { useLogout } from "@refinedev/core";
+import DarkModeOutlined from "@mui/icons-material/DarkModeOutlined";
+import LightModeOutlined from "@mui/icons-material/LightModeOutlined";
+import AppBar from "@mui/material/AppBar";
+import Avatar from "@mui/material/Avatar";
+import IconButton from "@mui/material/IconButton";
+import Stack from "@mui/material/Stack";
+import Toolbar from "@mui/material/Toolbar";
+import Typography from "@mui/material/Typography";
+import { useGetIdentity } from "@refinedev/core";
+import { HamburgerMenu, RefineThemedLayoutV2HeaderProps } from "@refinedev/mui";
+import React, { useContext } from "react";
+import { ColorModeContext } from "../../contexts/color-mode";
+// pm quan
+type IUser = {
+  id: number;
+  name: string;
+  avatar: string;
+};
 
-export const Header = () => {
-  const { mutate, isLoading } = useLogout();
+export const Header: React.FC<RefineThemedLayoutV2HeaderProps> = ({
+  sticky = true,
+}) => {
+  const { mode, setMode } = useContext(ColorModeContext);
+
+  const { data: user } = useGetIdentity<IUser>();
 
   return (
     <>
