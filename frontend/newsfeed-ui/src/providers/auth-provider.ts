7b932ef8a2386eca032c7ac1b94fc66364e2f245
Pmquan <quan111220@gmail.com>
2024-06-08 20:28:28 +0700

    login api, newsfeed

--- frontend/newsfeed-ui/src/providers/auth-provider.ts
+++ frontend/newsfeed-ui/src/providers/auth-provider.ts
@@ -1,34 +1,32 @@
 import { AuthProvider } from "@refinedev/core";
-
-const API_URL = "https://localhost:7152/api/v1";
-
+const API_URL = "https://localhost:44311/api/v1";
 export const authProvider: AuthProvider = {
+
   check: async () => {
     const token = localStorage.getItem("token");
 
     console.log("Token", token);
-    
+
     return { authenticated: Boolean(token) };
   },
-  login: async ({ email, password }) => {
-     const response = await fetch(`${API_URL}/authentication/login`, {
-        method: "POST",
-        body: JSON.stringify({ email, password }),
-        headers: {
-          "Content-Type": "application/json",
-        },
-     })
+  login: async (param: any) => {
+    const response = await fetch(`${API_URL}/authentication/login`, {
+      method: "POST",
+      body: JSON.stringify(param),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    })
+    const data = await response.json();
+    if (data?.data?.token) {
+      localStorage.setItem("token", data.data.token);
+      return { success: true };
+    }
 
-     const data = await response.json(); 
-     if (data?.data?.token) {
-        localStorage.setItem("token", data.data.token);
-        return { success: true };
-      }
-  
-      return { success: false };
+    return { success: false };
   },
   logout: async () => {
-    localStorage.removeItem("token");    
+    localStorage.removeItem("token");
     return { success: true };
   },
   onError: async (error) => {
--- frontend/newsfeed-ui/tailwind.config.js
+++ frontend/newsfeed-ui/tailwind.config.js
@@ -4,7 +4,11 @@ export default {
     "./src/**/*.{js,jsx,ts,tsx}",  
   ],  
   theme: {  
-    extend: {},  
+    extend: {
+      maxHeight: {
+        '128': '32rem',
+      }
+    },  
   },  
   plugins: [],  
 };
